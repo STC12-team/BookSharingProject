e8acf19ebfe2d068f6279ef85a1295de0b1d032c
Artem Zinoviev <arzinoviev@gmail.com>
2018-10-26 14:29:44 +0300

    Add all tests

--- src/test/java/ru/innopolis/stc12/booksharing/service/UserServiceTest.java
+++ src/test/java/ru/innopolis/stc12/booksharing/service/UserServiceTest.java
@@ -1,21 +1,25 @@
-package ru.innopolis.stc12.booksharing.model.dao;
+package ru.innopolis.stc12.booksharing.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import ru.innopolis.stc12.booksharing.model.connection.ConnectionManagerJdbcImpl;
+import ru.innopolis.stc12.booksharing.model.dao.UserDao;
+import ru.innopolis.stc12.booksharing.model.dao.UserDaoImpl;
+import ru.innopolis.stc12.booksharing.model.pojo.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-class UserDaoImplTest {
+public class UserServiceTest {
     @Mock
     ConnectionManagerJdbcImpl connectionManager;
 
@@ -28,36 +32,34 @@ class UserDaoImplTest {
     @Mock
     ResultSet resultSet;
 
-    private UserDaoImpl userDao;
+    UserDao userDao;
+    UserService userService;
 
     @BeforeEach
     void setUp() throws SQLException {
         initMocks(this);
         when(connectionManager.getConnection()).thenReturn(connection);
-        doNothing().when(connection).commit();
         when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
         doNothing().when(preparedStatement).setString(anyInt(), anyString());
-        when(preparedStatement.execute()).thenReturn(true);
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(false);
-        when(resultSet.getInt(anyInt())).thenReturn(1);
-        when(resultSet.getString(anyString())).thenReturn("dummy");
 
         userDao = new UserDaoImpl(connectionManager);
+        userService = new UserService(userDao);
     }
 
     @Test
-    void getUserByLogin() throws SQLException {
-        userDao.getUserByLogin("dummy");
+    void getUsers() throws SQLException {
+        userService.getUsers();
         verify(connection, times(1)).prepareStatement(anyString());
-        verify(preparedStatement, times(1)).setString(anyInt(), anyString());
         verify(resultSet, times(1)).next();
     }
 
     @Test
-    void getAllUsers() throws SQLException {
-        userDao.getAllUsers();
+    void getUserByLogin() throws SQLException {
+        userService.getUserByLogin("dummy");
         verify(connection, times(1)).prepareStatement(anyString());
+        verify(preparedStatement, times(1)).setString(anyInt(), anyString());
         verify(resultSet, times(1)).next();
     }
 }
