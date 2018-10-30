48ca10e629d8c92d1e805c8d63197c852b83254f
bass-2000 <bass-2000@yandex.ru>
2018-10-30 11:52:37 +0300

    deleted BookEditionsDao, UserDao

--- src/main/java/ru/innopolis/stc12/booksharing/controller/CatalogController.java
+++ src/main/java/ru/innopolis/stc12/booksharing/controller/CatalogController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.innopolis.stc12.booksharing.model.dao.BookEditionsDao;
-import ru.innopolis.stc12.booksharing.model.dao.UserDao;
 import ru.innopolis.stc12.booksharing.model.pojo.BookCopy;
 import ru.innopolis.stc12.booksharing.service.BookCopiesService;
 
@@ -15,22 +13,11 @@ import java.util.List;
 public class CatalogController {
     private static final String MESSAGE_ATTRIBUTE = "message";
     private BookCopiesService bookCopiesService;
-    private BookEditionsDao bookEditionsDao;
-    private UserDao userDao;
 
-    @Autowired
-    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
-    }
 
     @Autowired
     public void setBookCopiesService(BookCopiesService bookCopiesService) {
         this.bookCopiesService = bookCopiesService;
-    }
-
-    @Autowired
-    public void setBookEditionsDao(BookEditionsDao bookEditionsDao) {
-        this.bookEditionsDao = bookEditionsDao;
     }
 
     @GetMapping(value = "/catalog")
