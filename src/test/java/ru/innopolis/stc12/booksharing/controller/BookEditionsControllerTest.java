c3269cc0e86c18a043a74e60949deb8764e03e08
rmunirov <r.munirov.stc@innopolis.university>
2018-11-09 20:16:38 +0500

    1. added unit test

--- src/test/java/ru/innopolis/stc12/booksharing/controller/BookEditionsControllerTest.java
+++ src/test/java/ru/innopolis/stc12/booksharing/controller/BookEditionsControllerTest.java
@@ -5,27 +5,33 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.ui.Model;
+import ru.innopolis.stc12.booksharing.model.pojo.BookEdition;
+import ru.innopolis.stc12.booksharing.service.BookCopiesService;
 import ru.innopolis.stc12.booksharing.service.BookEditionsService;
+import ru.innopolis.stc12.booksharing.service.BookQueueService;
 import ru.innopolis.stc12.booksharing.service.PublisherService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 class BookEditionsControllerTest {
     @InjectMocks
     private BookEditionsController bookEditionsController;
-
-
     @Mock
     private Model model;
-
     @Mock
     private BookEditionsService bookEditionsService;
-
     @Mock
     private PublisherService publisherService;
+    @Mock
+    private BookEdition bookEdition;
+    @Mock
+    private BookCopiesService bookCopiesService;
+    @Mock
+    private BookQueueService bookQueueService;
 
     @BeforeEach
     void setUp() {
@@ -33,6 +39,8 @@ class BookEditionsControllerTest {
         bookEditionsController = new BookEditionsController();
         bookEditionsController.setBookEditionsService(bookEditionsService);
         bookEditionsController.setPublisherService(publisherService);
+        bookEditionsController.setBookCopiesService(bookCopiesService);
+        bookEditionsController.setBookQueueService(bookQueueService);
     }
 
     @Test
@@ -57,5 +65,18 @@ class BookEditionsControllerTest {
                         "title", "desc", "Amazon", "isbn", model
                 )
         );
+    }
+
+    @Test
+    void showBookEditionDescriptionPage() {
+        when(bookEditionsService.getById(anyInt())).thenReturn(bookEdition);
+        when(bookCopiesService.getBookCopyCountByBookEditionId(anyInt())).thenReturn(1);
+        when(bookCopiesService.getBookCopyCountByBookEditionIdInStatusFree(anyInt())).thenReturn(1);
+        when(bookQueueService.getUserCountByBookEditionId(anyInt())).thenReturn(1);
+        assertEquals("bookEditionDescription", bookEditionsController.showBookEditionDescriptionPage(1, model));
+        verify(model, times(1)).addAttribute("bookEdition", bookEdition);
+        verify(model, times(1)).addAttribute("countBookCopy", 1);
+        verify(model, times(1)).addAttribute("countBookCopyIsStatusFree", 1);
+        verify(model, times(1)).addAttribute("userCountInQueue", 1);
     }
 }
