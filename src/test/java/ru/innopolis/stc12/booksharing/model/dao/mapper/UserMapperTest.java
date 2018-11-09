91a5810f9c8cfbac2c9b9262a057c90f214605de
Вдовин Сергей <Vdovin.S.Yur@sberbank.ru>
2018-11-09 16:03:59 +0300

    Исправления по замечаниям

--- src/test/java/ru/innopolis/stc12/booksharing/model/dao/mapper/UserMapperTest.java
+++ src/test/java/ru/innopolis/stc12/booksharing/model/dao/mapper/UserMapperTest.java
@@ -8,18 +8,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserMapperTest {
+
     @Test
-    void mapRowTest() throws SQLException {
+    void mapRowTestFull() throws SQLException {
         ResultSet resultSetMock = Mockito.mock(ResultSet.class);
         Mockito.when(resultSetMock.getLong("id")).thenReturn((long) 1);
         Mockito.when(resultSetMock.getString("login")).thenReturn("Mocked login");
         Mockito.when(resultSetMock.getString("password")).thenReturn("Mocked password");
         Mockito.when(resultSetMock.getString("role_name")).thenReturn("Mocked role");
+        Mockito.when(resultSetMock.getInt("role_id")).thenReturn(2);
+        Mockito.when(resultSetMock.getInt("enabled")).thenReturn(1);
         UserMapper userMapper = new UserMapper();
         User userEdition = userMapper.mapRow(resultSetMock, 0);
-        User userExpected = new User(1, "Mocked login", "Mocked password", 0, "Mocked role", 0);
+        User userExpected = new User(1, "Mocked login", "Mocked password", 2, "Mocked role", 1);
         assertEquals(userExpected, userEdition);
     }
+
+    @Test
+    void mapRowTestNull() throws SQLException {
+        ResultSet resultSetMock = Mockito.mock(ResultSet.class);
+        UserMapper userMapper = new UserMapper();
+        User userEdition = userMapper.mapRow(resultSetMock, 0);
+        User userExpected = new User();
+        assertEquals(userExpected, userEdition);
+    }
+
 }
